e40ef3f88f296b58d3ce6ce257ff827061db5395
Gansaikhan Shur <gansaikhanshur@gmail.com>
2021-12-05 21:47:59 -0600

    Difficulty Easy

--- Easy/two_number_sum.py
+++ Easy/two_number_sum.py
@@ -1,7 +1,10 @@
 def twoNumberSum(array, targetSum):
 	output = []
+    #Loop through the main array
     for i in enumerate(array):
+        #Second loop to step to the right direction
 		for j in enumerate(array):
+            #make sure that we're not adding the same index twice
 			if i[0] != j[0]:
 				if i[1] + j[1] == targetSum:
 					return [i[1], j[1]]
