ea4df5f6b0a1f4bb49970bd728071beb2a828213
Gansaikhan Shur <gansaikhanshur@gmail.com>
2021-12-15 21:44:56 -0600

    Difficulty Easy

---
+++ Easy/Nth_Fibbonacci/nthFib.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Program {
+  public static int getNthFib(int n) {
+    if(n == 2){
+			return 1;
+		} else if(n == 1){
+			return 0;
+		} else {
+			return getNthFib(n - 1) + getNthFib(n - 2);
+		}
+  }
+}
+
+
