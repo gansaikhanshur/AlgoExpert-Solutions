6ddabe774aa1e29de6ea2f67c8747f7b361740e6
Gansaikhan Shur <gansaikhanshur@gmail.com>
2021-12-15 21:40:21 -0600

    Difficulty Easy

--- Easy/Nth_Fibbonacci/nthFib.py
+++ Easy/Nth_Fibbonacci/nthFib.py
@@ -22,3 +22,11 @@ def getNthFib(n):
 Method 2:
     A bit smarter way
 """
+
+def getNthFib(n):
+	if n == 1:
+		return 0
+	elif n == 2:
+		return 1
+	else:
+		return getNthFib(n - 1) + getNthFib(n - 2)
