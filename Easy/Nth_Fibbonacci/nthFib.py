3f24855502959083dd3f3c432ae154fcbb3d901f
Gansaikhan Shur <gansaikhanshur@gmail.com>
2021-12-15 21:38:18 -0600

    Difficulty Easy

---
+++ Easy/Nth_Fibbonacci/nthFib.py
@@ -0,0 +1,24 @@
+"""
+Method 1:
+    Brute Force
+"""
+
+def getNthFib(n):
+	fib_seq = [0]
+
+	if n == 1:
+		return 0
+	
+    for i in range(0, n):
+        if i == 0 or i == 1:
+            fib_seq.append(1)
+        else:
+            fib_seq.append((fib_seq[i] + fib_seq[i - 1]))
+
+    fib_seq = fib_seq[:-1]
+    return fib_seq[n - 2] + fib_seq[n - 3]
+
+"""
+Method 2:
+    A bit smarter way
+"""
